0895cefd47454ee28474372c238180ba3f19087e
Talha <108174098+anakin-dabir@users.noreply.github.com>
2024-02-28 06:51:50 +0500

    Update baseQuery.jsx

--- client/src/services/baseQuery.jsx
+++ client/src/services/baseQuery.jsx
@@ -5,7 +5,7 @@ import {fetchBaseQuery} from '@reduxjs/toolkit/query/react';
 import Cookies from 'js-cookie';
 
 const baseQuery = fetchBaseQuery({
-  baseUrl: 'https://repulsive-tan-gown.cyclic.app',
+  baseUrl: 'https://wons-dashboard.cyclic.app',
   credentials: 'include',
   prepareHeaders: headers => {
     const token = Cookies.get('jwt_token');
